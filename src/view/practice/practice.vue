f0c44b75a99b52699855b343f26df0d269dc6722
flyfig <1130041599@qq.com>
2019-03-25 23:02:04 +0800

    我

---
+++ src/view/practice/practice.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{name}}</div>
+</template>
+
+<script>
+    export default {
+        name: "practice"
+    }
+</script>
+
+<style scoped src="../../css/practice/practice.css">
+
+</style>
